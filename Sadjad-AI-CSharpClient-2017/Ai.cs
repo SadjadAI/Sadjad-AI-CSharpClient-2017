5b4aacc70f135c62a407ab111809d92ebc2350e4
Taeyelor <Taeyelor@gmail.com>
2017-04-23 01:37:03 +0430

    Run and test Ver1

---
+++ Sadjad-AI-CSharpClient-2017/Ai.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sadjad_AI_CSharpClient_2017
+{
+    class Ai
+    {
+        public String ServerAdr = "http://localhost:5000/game";
+        public Int32 Token = 123;
+
+        public void doTurn(ref Int32[] touch)
+        {
+            Random rnd = new Random();
+
+            touch[0] = rnd.Next(0, 9);
+            touch[1] = rnd.Next(0, 9);
+        }
+    }
+}
--- Sadjad-AI-CSharpClient-2017/Program.cs
+++ Sadjad-AI-CSharpClient-2017/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quobject.SocketIoClientDotNet.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
 
 namespace Sadjad_AI_CSharpClient_2017
 {
@@ -10,6 +13,27 @@ namespace Sadjad_AI_CSharpClient_2017
     {
         static void Main(string[] args)
         {
+            Ai AI = new Ai();
+
+            var socket = IO.Socket(AI.ServerAdr);
+
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                Console.WriteLine("Connected...");
+                socket.Emit("init", AI.Token);
+            });
+
+            while(true)
+            {
+                socket.On("result", (data) =>
+                {
+                    Int32[] touch = new Int32[2];
+
+                    AI.doTurn(ref touch);
+
+                    socket.Emit("touch", AI.Token, touch[0], touch[1]);
+                });
+            }
         }
     }
 }
